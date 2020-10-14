2b365c9b3c22f6897446f26ef8d62982ae8a499e
Felipe Pepe <FelipePepe@gmail.com>
2020-10-14 09:18:07 +0200

    Refactoring code

--- MergePDF_WF/Form1.cs
+++ MergePDF_WF/Form1.cs
@@ -1,18 +1,7 @@
-﻿using iText.Kernel.Pdf;
-using iText.Kernel.Pdf.Canvas;
-using iText.Kernel.Utils;
-using iText.Layout;
-using System;
+﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.ListView;
+using MyPdfTools;
 
 namespace MergePDF_WF
 {
@@ -40,24 +29,6 @@ namespace MergePDF_WF
             }
         }
 
-        private void MergePDF(string dest, string[] Files)
-        {
-            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
-            PdfMerger merger = new PdfMerger(pdf);
-
-            foreach (string filePDF in Files)
-            {
-
-                //Add pages from the first document
-                PdfDocument firstSourcePdf = new PdfDocument(new PdfReader(filePDF));
-                merger.Merge(firstSourcePdf, 1, firstSourcePdf.GetNumberOfPages());
-                firstSourcePdf.Close();
-            }
-
-
-            pdf.Close();
-        }
-
         private void btnFicheroDestino_Click(object sender, EventArgs e)
         {
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
@@ -80,7 +51,7 @@ namespace MergePDF_WF
                 }
 
             }
-            MergePDF(txtFicheroDestino.Text, (string[])lstFiles.ToArray(typeof(string)));
+            PdfTools.MergePDF(txtFicheroDestino.Text, (string[])lstFiles.ToArray(typeof(string)));
         }
 
         private void btnRotar_Click(object sender, EventArgs e)
@@ -93,7 +64,7 @@ namespace MergePDF_WF
             if (lstFicheros.SelectedItems.Count == 1)
             {
                 string ficheroOrigen = lstFicheros.SelectedItems[0].SubItems[1].Text;
-                RotarPDF(txtFicheroDestino.Text, ficheroOrigen, int.Parse(txtAngulo.Text));
+                PdfTools.RotarPDF(txtFicheroDestino.Text, ficheroOrigen, int.Parse(txtAngulo.Text));
             }
             else
             {
@@ -105,30 +76,11 @@ namespace MergePDF_WF
                 {
                     lstFicheros.Items[0].Selected = true;
                     string ficheroOrigen = lstFicheros.SelectedItems[0].SubItems[1].Text;
-                    RotarPDF(txtFicheroDestino.Text, ficheroOrigen, int.Parse(txtAngulo.Text));
+                    PdfTools.RotarPDF(txtFicheroDestino.Text, ficheroOrigen, int.Parse(txtAngulo.Text));
                 }
             }
         }
 
-        private void RotarPDF(string ficheroDestino, string ficheroOrigen, int angulo)
-        {
-            PdfDocument pdfDoc = new PdfDocument(new PdfReader(ficheroOrigen), new PdfWriter(ficheroDestino));
-            int margin = 72;
-            for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
-            {
-                PdfPage page = pdfDoc.GetPage(i);
-                // change page size
-                iText.Kernel.Geom.Rectangle mediaBox = page.GetMediaBox();
-                iText.Kernel.Geom.Rectangle newMediaBox = new iText.Kernel.Geom.Rectangle((int)mediaBox.GetLeft() - margin, (int)mediaBox.GetBottom() - margin, (int)mediaBox.GetWidth() + margin * 2, (int)mediaBox.GetHeight() + margin * 2);
-                page.SetMediaBox(newMediaBox);
-                // add border
-                PdfCanvas over = new PdfCanvas(page);
-                over.Rectangle(mediaBox.GetLeft(), mediaBox.GetBottom(), mediaBox.GetWidth(), mediaBox.GetHeight());
-                over.Stroke();
-                // change rotation of the even pages
-                page.SetRotation(angulo);
-            }
-            pdfDoc.Close();
-        }
+       
     }
 }
---
+++ PdfTools/PdfTool.cs
@@ -0,0 +1,48 @@
+﻿namespace MyPdfTools
+{
+    using iText.Kernel.Pdf;
+    using iText.Kernel.Pdf.Canvas;
+    using iText.Kernel.Utils;
+
+    public class PdfTools
+    {
+        public static void MergePDF(string dest, string[] Files)
+        {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+            PdfMerger merger = new PdfMerger(pdf);
+
+            foreach (string filePDF in Files)
+            {
+
+                //Add pages from the first document
+                PdfDocument firstSourcePdf = new PdfDocument(new PdfReader(filePDF));
+                merger.Merge(firstSourcePdf, 1, firstSourcePdf.GetNumberOfPages());
+                firstSourcePdf.Close();
+            }
+
+
+            pdf.Close();
+        }
+
+        public static void RotarPDF(string ficheroDestino, string ficheroOrigen, int angulo)
+        {
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(ficheroOrigen), new PdfWriter(ficheroDestino));
+            int margin = 72;
+            for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+            {
+                PdfPage page = pdfDoc.GetPage(i);
+                // change page size
+                iText.Kernel.Geom.Rectangle mediaBox = page.GetMediaBox();
+                iText.Kernel.Geom.Rectangle newMediaBox = new iText.Kernel.Geom.Rectangle((int)mediaBox.GetLeft() - margin, (int)mediaBox.GetBottom() - margin, (int)mediaBox.GetWidth() + margin * 2, (int)mediaBox.GetHeight() + margin * 2);
+                page.SetMediaBox(newMediaBox);
+                // add border
+                PdfCanvas over = new PdfCanvas(page);
+                over.Rectangle(mediaBox.GetLeft(), mediaBox.GetBottom(), mediaBox.GetWidth(), mediaBox.GetHeight());
+                over.Stroke();
+                // change rotation of the even pages
+                page.SetRotation(angulo);
+            }
+            pdfDoc.Close();
+        }
+    }
+}
